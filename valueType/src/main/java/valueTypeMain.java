620f304d822f3acb4f8de73cac7d8d46f63c1693
vito <ckdghk987@gmail.com>
2021-05-04 19:16:32 +0900

    create valueType module

---
+++ valueType/src/main/java/valueTypeMain.java
@@ -0,0 +1,12 @@
+public class valueTypeMain {
+
+    public static void main(String[] args) {
+
+        Integer a = 10;
+        Integer b = a;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
